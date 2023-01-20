54f07f77e66a2fe306473b62eabc16f06a9b15f7
Emanuel Zani <manuzani10@gmail.com>
2023-01-19 22:24:55 -0300

    changed src/components/Card.jsx: pull content from app/detailBook/page.jsx and push in components/card

--- novelty-books/src/components/Card.jsx
+++ novelty-books/src/components/Card.jsx
@@ -1,17 +1,17 @@
-const Card = ({title, image, price}) => (
+const Card = ({title, image, price,author}) => (
     <>
         <div class="col mb-5">
                         <div class="card h-100">
                             
                             <div class="badge bg-dark text-white position-absolute" style={{top: 10, right: 10}}>Sale</div>
                             
-                            <img class="card-img-top" src={image} alt="..." />
+                            <img class="card-img-top" style={{height: 270, width: 223}} src={image} alt="..." />
                             
                             <div class="card-body p-4">
                                 <div class="text-center">
                                     
                                     <h5 class="fw-bolder">{title}</h5>
-                                    <p>Author's Name</p>
+                                    <p>{author}</p>
                                     
                                     <div class="d-flex justify-content-center small text-warning mb-2">
                                         <div class="bi-star-fill"></div>
@@ -20,7 +20,7 @@ const Card = ({title, image, price}) => (
                                         
                                     </div>
                                     
-                                    <span >${price}</span>
+                                    <span >$ {price}</span>
                                     
                                 </div>
                             </div>
