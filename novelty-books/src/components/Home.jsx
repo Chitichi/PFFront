e847c0c23c2148349c3bb877cf99b3efd41a3f95
Troy Pernía <troy00pernia@gmail.com>
2023-01-21 15:10:24 -0400

    chaged components/Home: add function changeType. Add tags: <input> (line 40), <select> (line 48)

--- novelty-books/src/components/Home.jsx
+++ novelty-books/src/components/Home.jsx
@@ -11,6 +11,10 @@ function Landing() {
   const [searchInput, setSearchInput] = useState("");
   const [typeSearch, setTypeSearch] = useState('');
 
+  const changeType = (e) => {
+    e.preventDefault();
+    setType(e.target.value);
+  };
 
   return (
     <>
@@ -28,6 +32,28 @@ function Landing() {
       <section class="py-5 bg-light">
         <div class="container px-4 px-lg-5 mt-5">
           <h2 class="fw-bolder mb-4">Popular books</h2>
+
+          <div style={{ margin: 5 }} class="d-flex flex-row-reverse">
+            <nav class="navbar bg-body-tertiary">
+              <div class="container-fluid">
+                <form class="d-flex" role="search">
+                  <input
+                    class="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    onChange={(e) => setSearch(e.target.value)}
+                  ></input>
+
+                  <select name="filter" onChange={(e) => changeType(e)}>
+                    <option value="title">Title</option>
+                    <option value="author">Author</option>
+                  </select>
+                </form>
+              </div>
+            </nav>
+          </div>
+
           <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
             {books.map((book) => {
               return (
