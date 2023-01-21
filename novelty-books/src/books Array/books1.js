4893c35eab9338764943a7a2c2ac8015f6ff73fb
Jaider <jaidercueto.1010@gmail.com>
2023-01-21 17:40:03 -0500

    modified src/books Array/books1.js : property 'publisher' added

--- novelty-books/src/books Array/books1.js
+++ novelty-books/src/books Array/books1.js
@@ -7,7 +7,8 @@ const books= [
         price: 7.99,
         isbn13: "100",
         description: "Rescued from the outrageous neglect of his aunt and uncle, a young boy with a great destiny proves his worth while attending Hogwarts School for Witchcraft and Wizardry.",
-        genre: ["Fantasy"]
+        genre: ["Fantasy"],
+        publisher: ["Quirk Books"]
     },
     {
         title: "The Lord of the Rings Illustrated",
@@ -17,7 +18,8 @@ const books= [
         price: 39.19,
         isbn13: 110,
         description: "This one-volume, jacketed hardcover edition contains the complete text, fully corrected and reset, which is printed in red and black and features, for the very first time, thirty color illustrations, maps and sketches drawn by Tolkien himself as he composed this epic work. These include the pages from the Book of Mazarbul, marvelous facsimiles created by Tolkien to accompany the famous ‘Bridge of Khazad-dum’ chapter. Also appearing are two removable fold-out maps drawn by Christopher Tolkien revealing all the detail of Middle-earth.",
-        genre: ["Fantasy"]
+        genre: ["Fantasy"],
+        publisher: ["OSU Press"]
     },
     {
         title: "Dune",
@@ -27,7 +29,8 @@ const books= [
         price: 9.89,
         isbn13: 120,
         description: "Set on the desert planet Arrakis, Dune is the story of the boy Paul Atreides, heir to a noble family tasked with ruling an inhospitable world where the only thing of value is the “spice” melange, a drug capable of extending life and enhancing consciousness. Coveted across the known universe, melange is a prize worth killing for....",
-        genre: ["Sci-Fiction"]
+        genre: ["Sci-Fiction"],
+        publisher: ["OSU Press"]
     },
     {
         title: "The Martian",
@@ -37,7 +40,8 @@ const books= [
         price: 10.41,
         isbn13: 130,
         description: "After a dust storm nearly kills him and forces his crew to evacuate while thinking him dead, Mark finds himself stranded and completely alone with no way to even signal Earth that he’s alive—and even if he could get word out, his supplies would be gone long before a rescue could arrive.",
-        genre: ["Sci-Fiction"]
+        genre: ["Sci-Fiction"],
+        publisher: ["OSU Press"]
     },
     {
         title: "Foundation",
@@ -47,7 +51,8 @@ const books= [
         price: 18.30,
         isbn13: 140,
         description: "The Galactic Empire has prospered for twelve thousand years. Nobody suspects that the heart of the thriving Empire is rotting, until psychohistorian Hari Seldon uses his new science to foresee its terrible fate.",
-        genre: ["Sci-Fiction"]
+        genre: ["Sci-Fiction"],
+        publisher: ["Quirk Books"]
     },
     {
         title: "The Way of Kings",
@@ -57,7 +62,8 @@ const books= [
         price: 9.89,
         isbn13: 150,
         description: "Roshar is a world of stone and storms. Uncanny tempests of incredible power sweep across the rocky terrain so frequently that they have shaped ecology and civilization alike. Animals hide in shells, trees pull in branches, and grass retracts into the soilless ground. Cities are built only where the topography offers shelter.",
-        genre: ["Fantasy"]
+        genre: ["Fantasy"],
+        publisher: ["OSU Press"]
     },
     {
         title: "The Shining",
@@ -67,7 +73,8 @@ const books= [
         price: 8.99,
         isbn13: 160,
         description: "Jack Torrance’s new job at the Overlook Hotel is the perfect chance for a fresh start. As the off-season caretaker at the atmospheric old hotel, he’ll have plenty of time to spend reconnecting with his family and working on his writing. But as the harsh winter weather sets in, the idyllic location feels ever more remote . . . and more sinister. And the only one to notice the strange and terrible forces gathering around the Overlook is Danny Torrance, a uniquely gifted five-year-old.",
-        genre: ["Horror"]
+        genre: ["Horror"],
+        publisher: ["Quirk Books"]
     },
     {
         title: "The Call of Cthulhu",
@@ -77,7 +84,8 @@ const books= [
         price: 7.99,
         isbn13: 170,
         description: "The creature is described as “a monster of vaguely anthropoid outline, but with an octopus-like head whose face was a mass of feelers, a scaly, rubbery-looking body, prodigious claws on hind and fore feet, and long, narrow wings behind.” It is said to be so terrible to behold that it destroys the sanity of those who ...",
-        genre: ["Horror"]
+        genre: ["Horror"],
+        publisher: ["Quirk Books"]
     },
     {
         title: "The Fall of the House of Usher",
@@ -87,7 +95,8 @@ const books= [
         price: 4.45,
         isbn13: 180,
         description: "Edgar Allan Poe is one of the true greats, a masterful poet and word-smith, Edgar is considered by many to be one of the greatest poets and writers of all time. This is a wonderful short story by one of the worlds most respected writers, a wonderful book to add to any collection.",
-        genre: ["Horror"]
+        genre: ["Horror"],
+        publisher: ["Wiley"]
     },
     {
         title: "1984",
@@ -97,7 +106,8 @@ const books= [
         price: 7.48,
         isbn13: 190,
         description: "Winston Smith toes the Party line, rewriting history to satisfy the demands of the Ministry of Truth. With each lie he writes, Winston grows to hate the Party that seeks power for its own sake and persecutes those who dare to commit thoughtcrimes. But as he starts to think for himself, Winston can’t escape the fact that Big Brother is always watching...",
-        genre: ["Sci-Fiction"]
+        genre: ["Sci-Fiction"],
+        publisher: ["Wiley"]
     },
 ];
 
