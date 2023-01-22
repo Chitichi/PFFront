52bd3b63f40d7982f67d681dd969808732130437
Troy Pernía <troy00pernia@gmail.com>
2023-01-21 21:22:04 -0400

    change components/Home app/page: moved tag <header> (line 74) from /components/Home to app/page (line 6)

--- novelty-books/src/app/page.jsx
+++ novelty-books/src/app/page.jsx
@@ -3,7 +3,18 @@ import Landing from "@/components/Home"
 export default function Home() {
   return (
     <>
-      <Landing/>
+      <header class="bg-dark py-5">
+        <div class="container px-4 px-lg-5 my-5">
+          <div class="text-center text-white">
+            <h1 class="display-4 fw-bolder">Novelty Books</h1>
+            <p class="lead fw-normal text-white-50 mb-0">
+              With this shop hompeage template
+            </p>
+          </div>
+        </div>
+      </header>
+
+      <Landing />
     </>
   )
 }
--- novelty-books/src/components/Home.jsx
+++ novelty-books/src/components/Home.jsx
@@ -71,17 +71,6 @@ function Landing() {
 
   return (
     <>
-      <header class="bg-dark py-5">
-        <div class="container px-4 px-lg-5 my-5">
-          <div class="text-center text-white">
-            <h1 class="display-4 fw-bolder">Novelty Books</h1>
-            <p class="lead fw-normal text-white-50 mb-0">
-              With this shop hompeage template
-            </p>
-          </div>
-        </div>
-      </header>
-
       <section class="py-5 bg-light">
         <div class="container px-4 px-lg-5 mt-5">
           <h2 class="fw-bolder mb-4">Popular books</h2>
