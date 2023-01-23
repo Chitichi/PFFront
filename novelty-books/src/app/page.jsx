5ad6ab788f98a935e4dcb5b0ba2fb0e039ee881d
Troy Pernía <troy00pernia@gmail.com>
2023-01-23 10:58:30 -0400

    changed app/page: delete tag <p> (line 18)

--- novelty-books/src/app/page.jsx
+++ novelty-books/src/app/page.jsx
@@ -7,7 +7,7 @@ const fetchBooks = () => {
 }
 
 export default async function Home() {
-
+  
   const books = await fetchBooks()
 
   return (
@@ -16,9 +16,6 @@ export default async function Home() {
         <div class="container px-4 px-lg-5 my-5">
           <div class="text-center text-white">
             <h1 class="display-4 fw-bolder">Novelty Books</h1>
-            <p class="lead fw-normal text-white-50 mb-0">
-              With this shop hompeage template
-            </p>
           </div>
         </div>
       </header>
@@ -27,3 +24,4 @@ export default async function Home() {
     </>
   )
 }
+
