f6dbaec7b05c3b44dbd590c51b7c6ac8fb84717e
Emanuel Zani <manuzani10@gmail.com>
2023-01-25 21:29:05 -0300

    install bootstrap icons

--- novelty-books/src/app/layout.jsx
+++ novelty-books/src/app/layout.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
