0d793b2912a307a67f46af7a0a77f1344b5d169a
Emanuel Zani <manuzani10@gmail.com>
2023-01-26 12:12:58 -0300

    changed src/app/layout.jsx: add component StateContext,cover the html

--- novelty-books/src/app/layout.jsx
+++ novelty-books/src/app/layout.jsx
@@ -2,10 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { StateContext } from 'context/StateContext'
 
 export default function RootLayout({ children }) {
 
   return (
+    <StateContext>
+
+    
     <html lang="en">
       <head />
       <body>
@@ -14,5 +18,6 @@ export default function RootLayout({ children }) {
         <Footer/>
         </body>
     </html>
+    </StateContext>
   )
 }
