148ada2037564d46bb66a9237a2c92c29ca71ca9
Troy Pernía <troy00pernia@gmail.com>
2023-01-29 14:41:39 -0400

    changed components/Navbar: add removed "user" in localSotrage (line 25)

--- novelty-books/src/components/Navbar.jsx
+++ novelty-books/src/components/Navbar.jsx
@@ -6,29 +6,30 @@ import { useRouter } from "next/navigation";
 
 const Navbar = () => {
   const pathName = usePathname();
- // const {profile} = params;
+  // const {profile} = params;
 
-  const { totalQuantities , user, setUser } = useStateContext();
+  const { totalQuantities, user, setUser } = useStateContext();
   const router = useRouter();
- //const user = pathName.includes("profile") ? pathName.slice(9) : null
-  function redirectHome(){
-   if (user.name){
-    router.push(`/profile/${user.name}`);
-   } else {
-    router.push("/");
-   }
+  //const user = pathName.includes("profile") ? pathName.slice(9) : null
+  function redirectHome() {
+    if (user.name) {
+      router.push(`/profile/${user.name}`);
+    } else {
+      router.push("/");
+    }
   }
 
 
-  function logout(){
+  function logout() {
     setUser({});
-  } 
+    localStorage.removeItem("user")
+  }
 
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container px-4 px-lg-5">
-          <img  style={{height: 35 }} src="brand.png" alt=""/>
+        <img style={{ height: 35 }} src="brand.png" alt="" />
         <a className="navbar-brand" href="#!">
           Novelty Books
         </a>
@@ -46,33 +47,33 @@ const Navbar = () => {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
             <li class="nav-item">
-              <button 
-              onClick={redirectHome}
-              class="btn btn-outline-dark"
+              <button
+                onClick={redirectHome}
+                class="btn btn-outline-dark"
               >
                 <p class="nav-link active" aria-current="page"  >
                   Home
                 </p>
               </button>
             </li>
-           
+
           </ul>
           {
-             Object.keys(user).length === 0 ? // si no estamos logueados mostramos el boton para crear cuenta.
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-            <li class="nav-item">
-              <Link  href="/SignUp">
-                <p className="nav-link active" aria-current="page"  >
-                  SignUp
-                </p>
-              </Link>
-            </li>
-           
-          </ul>
-          </div> : null
+            Object.keys(user).length === 0 ? // si no estamos logueados mostramos el boton para crear cuenta.
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                  <li class="nav-item">
+                    <Link href="/SignUp">
+                      <p className="nav-link active" aria-current="page"  >
+                        SignUp
+                      </p>
+                    </Link>
+                  </li>
+
+                </ul>
+              </div> : null
           }
-       
+
 
           <form className="d-flex">
             <Link href="/cart">
@@ -84,43 +85,43 @@ const Navbar = () => {
             </Link>
             {
               pathName === "/login" || pathName === "/SignUp" ? null :
-              pathName !== "/" ?
-              <Link href="/">
-                  <button  
-                    onClick={logout}
-                    style={{marginLeft: 10}}
-                    className="btn btn-outline-dark" 
-                    type="submit">
-                     Logout
-                  </button>
-            </Link> :
-             <Link href="/login">
-             <button   
-                  style={{marginLeft: 10}}
-                  className="btn btn-outline-dark" 
-                  type="submit">
-               Login
-             </button>
-           </Link>
+                pathName !== "/" ?
+                  <Link href="/">
+                    <button
+                      onClick={logout}
+                      style={{ marginLeft: 10 }}
+                      className="btn btn-outline-dark"
+                      type="submit">
+                      Logout
+                    </button>
+                  </Link> :
+                  <Link href="/login">
+                    <button
+                      style={{ marginLeft: 10 }}
+                      className="btn btn-outline-dark"
+                      type="submit">
+                      Login
+                    </button>
+                  </Link>
             }
-          {
-          pathName === `/profile/${user.name}`? 
-          <Link href="/miPerfil">
-            <button>
-              Mi Perfil
-            </button>
-          </Link> : null
-          
+            {
+              pathName === `/profile/${user.name}` ?
+                <Link href="/miPerfil">
+                  <button>
+                    Mi Perfil
+                  </button>
+                </Link> : null
 
 
-          }
-           
-            
+
+            }
+
+
           </form>
         </div>
       </div>
     </nav>
   )
 };
-  
-  export default Navbar;
+
+export default Navbar;
