1f98e36f851900f06cd67bb0034f6ae805c070e5
Troy Pernía <troy00pernia@gmail.com>
2023-01-29 17:37:04 -0400

    changed app/login: changed class for className

--- novelty-books/src/app/login/page.jsx
+++ novelty-books/src/app/login/page.jsx
@@ -39,7 +39,6 @@ function Login() {
                   timer: 3000
               })
             } else if (typeof(data) === "object"){
-                console.log(data)
                 setUser(data);
                 saveLocalStorage(data)
                 router.push(`/profile/${data.name}`);
@@ -57,40 +56,40 @@ function Login() {
     return (
         <div>
         <form onSubmit={handleSubmit} className={style.formulario}>
-            <div class="form-group">
+            <div className="form-group">
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" 
-                       class="form-control" 
+                       className="form-control" 
                        id="exampleInputEmail1" 
                        aria-describedby="emailHelp" 
                        placeholder="Enter email"
                        value={emailUser}
                        onChange= {(e)=> setEmailUser(e.target.value)}/>
                 <small id="emailHelp" 
-                       class="form-text text-muted">We'll never share your email with anyone else.</small>
+                       className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label for="exampleInputPassword1">Password</label>
                 <input type="password" 
-                       class="form-control" 
+                       className="form-control" 
                        id="exampleInputPassword1" 
                        placeholder="Password"
                        value={passUser}
                        onChange= {(e)=> setPassUser(e.target.value)}/>
             </div>
-            <div class="form-group form-check">
+            <div className="form-group form-check">
                 <input type="checkbox" 
-                       class="form-check-input" 
+                       className="form-check-input" 
                        id="exampleCheck1"/>
-                <label class="form-check-label" 
+                <label className="form-check-label" 
                        for="exampleCheck1">Check me out</label>
             </div>
-            <div class="text-center">
+            <div className="text-center">
                 
                 
             </div>
         
-            <div class="text-center">
+            <div className="text-center">
                 <button 
                 className={style.button} 
                 type="submit"
