c702e64d6f987f81386093f6b5c26d1cfac87bd8
Troy Pernía <troy00pernia@gmail.com>
2023-01-30 11:42:55 -0400

    changes app/layout: format the document

--- novelty-books/src/app/layout.jsx
+++ novelty-books/src/app/layout.jsx
@@ -3,21 +3,8 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import { StateContext } from 'context/StateContext'
-import { useStateContext } from "../../context/StateContext";
 
 export default function RootLayout({ children }) {
-
-  function updateData() {
-    if (typeof window !== "undefined") {
-      const { user, setUser } = useStateContext();
-      if (!user.name) {
-        const userLocalStorage = JSON.parse(localStorage.getItem("user"))
-        if (userLocalStorage) setUser(userLocalStorage)
-      }
-    }
-  }
-
-  updateData()
 
   return (
     <StateContext>
