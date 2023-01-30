e7e9ccaa4f478ab03563421caf650dcb9d595052
Troy Pernía <troy00pernia@gmail.com>
2023-01-29 22:19:00 -0400

    changed app/layout: add function "updateData" (line 10)

--- novelty-books/src/app/layout.jsx
+++ novelty-books/src/app/layout.jsx
@@ -3,21 +3,32 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import { StateContext } from 'context/StateContext'
+import { useStateContext } from "../../context/StateContext";
 
 export default function RootLayout({ children }) {
 
+  function updateData() {
+    if (typeof window !== "undefined") {
+      const { user, setUser } = useStateContext();
+      if (!user.name) {
+        const userLocalStorage = JSON.parse(localStorage.getItem("user"))
+        if (userLocalStorage) setUser(userLocalStorage)
+      }
+    }
+  }
+
+  updateData()
+
   return (
     <StateContext>
-
-    
-    <html lang="en">
-      <head />
-      <body>
-        <Navbar />
-        {children}
-        <Footer/>
+      <html lang="en">
+        <head />
+        <body>
+          <Navbar />
+          {children}
+          <Footer />
         </body>
-    </html>
+      </html>
     </StateContext>
   )
 }
