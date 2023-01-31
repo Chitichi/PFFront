a96b3448bbb912ac99d2a0c73ee0db966601f08b
Troy Pernía <troy00pernia@gmail.com>
2023-01-31 19:17:11 -0400

    changed components/Listusers: add table to render

--- novelty-books/src/components/ListUsers.jsx
+++ novelty-books/src/components/ListUsers.jsx
@@ -1,8 +1,28 @@
-function ListUsers() {
+"use client"
+import React from "react"
+import ShowList from "./ShowList"
+
+function ListUsers({listUsers}) {
+
+    const [list, setList] = React.useState(listUsers)
 
     return (
         <>
             <h3>Lista de usuarios</h3>
+            <input type="search" placeholder="Search"></input>
+            <select>
+                <option hidden></option>
+                <option >id</option>
+                <option>nombre</option>
+                <option>correo</option>
+            </select>
+            <label>Admin: </label>
+            <select>
+                <option>True</option>
+                <option>False</option>
+            </select>
+           
+            <ShowList listUsers={listUsers}/>
         </>
     )
 }
