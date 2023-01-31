46c09ddadb89d99ea9910627736b1084975f7ac0
Troy Pernía <troy00pernia@gmail.com>
2023-01-31 19:16:10 -0400

    changed components/AdminTools: add local state "data" (line 8) and useEffect (line 34)

--- novelty-books/src/components/AdminTools.jsx
+++ novelty-books/src/components/AdminTools.jsx
@@ -5,24 +5,40 @@ import ListUsers from "./ListUsers"
 function AdminTools() {
 
     const [tool, setTool] = React.useState("")
+    const [data, setData] = React.useState([])
 
+    const mount = React.useRef(true)
+    
     function handleTool(e) {
-        console.log(e.target.name)
         setTool(e.target.name)
+        mount.current=false
     }
 
     function bringData() {
+        // fetch(process.env.RUTA_BACK+"/users")
+        //     .then(response => response.json())
+        //     .then(data => data.map(user => ({
+        //         id: user._id,
+        //         name: user.name,
+        //         rol: user.rolAdmin
+        //     })))
         fetch(process.env.RUTA_BACK+"/users")
             .then(response => response.json())
-            .then(data => data.map(user => ({
-                id: user._id,
-                name: user.name,
-                rol: user.rolAdmin
-            })))
+            .then(data => setData(data))
     }
 
     function usersManager() {
         
+    }
+
+    React.useEffect(() => {
+        if(!mount.current) {    
+            bringData()
+        }
+    },[tool])
+
+    function showData() {
+        console.log(data)
     }
 
     return (
@@ -32,7 +48,8 @@ function AdminTools() {
             <button onClick={handleTool} name="books">Libros</button>
             <button onClick={handleTool} name="orders">Ordenes</button>
             <button onClick={handleTool} name="statistics">Estadisticas</button>
-            {tool==="users"?<ListUsers/>:null}
+            {tool==="users"?<ListUsers listUsers={data}/>:null}
+            <button onClick={showData} >Show Data</button>
             
         </>
     )
