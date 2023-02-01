ef60d882c402a9755453860d82d1ef9deb9d798d
Troy Pernía <troy00pernia@gmail.com>
2023-02-01 10:03:04 -0400

    changed components/ShowListUsers: add styles to table"

--- novelty-books/src/components/ShowListUsers.jsx
+++ novelty-books/src/components/ShowListUsers.jsx
@@ -1,22 +1,31 @@
-function ShowListUsers({listUsers, goDetailUser}) {
+function ShowListUsers({ listUsers, goDetailUser }) {
     return (
         <>
-            <table>
-                <tr>
-                    <th>id</th>
-                    <th>nombre</th>
-                    <th>correo</th>
-                    <th>admin</th>
-                </tr>
-                {listUsers.map((user, index) => (
-                    <tr key={index} onClick={(e) => {goDetailUser(user._id)}}>
-                        <td>{user._id}</td>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                        <td>{`${user.rolAdmin}`}</td>
-                    </tr>
-                ))}
-            </table>
+            <div className="container px-4 px-lg-5 my-5">
+                <table className="table table-striped table-hover table-bordered ">
+                
+                    <thead className="table-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">id</th>
+                            <th scope="col">nombre</th>
+                            <th scope="col">correo</th>
+                            <th scope="col">admin</th>
+                        </tr>
+                    </thead>
+                    <tbody className="table-group-divider">
+                        {listUsers.map((user, index) => (
+                            <tr key={index} onClick={(e) => { goDetailUser(user._id) }} scope="row">
+                                <th scope="row">{index + 1}</th>
+                                <td>{user._id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{`${user.rolAdmin}`}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
