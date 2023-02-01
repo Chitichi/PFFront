48656a8f8f835a6d10cd598fd75a422713a5aed2
Troy Pernía <troy00pernia@gmail.com>
2023-02-01 10:10:43 -0400

    changed components/ListUsers: add function handleSearch (line 16)

--- novelty-books/src/components/ListUsers.jsx
+++ novelty-books/src/components/ListUsers.jsx
@@ -1,34 +1,47 @@
 "use client"
 import React from "react"
 import ShowListUsers from "./ShowListUsers"
-import {useRouter} from "next/navigation"
+import { useRouter } from "next/navigation"
 
-function ListUsers({listUsers}) {
+function ListUsers({ listUsers }) {
 
     const [list, setList] = React.useState(listUsers)
+    const [inputSearch, setInputSearch] = React.useState("")
     const router = useRouter()
 
     function goDetailUser(id) {
         router.push(`/admin/${id}`)
     }
 
+    function handleSearch(event){
+        const input = event.target.value
+        setInputSearch(input)
+        filter(input)
+    }
+
+    function filter(input){
+        
+    }
+
     return (
         <>
-            <h3>Lista de usuarios</h3>
-            <input type="search" placeholder="Search"></input>
-            <select>
-                <option hidden></option>
-                <option >id</option>
-                <option>nombre</option>
-                <option>correo</option>
-            </select>
-            <label>Admin: </label>
-            <select>
-                <option>True</option>
-                <option>False</option>
-            </select>
-           
-            <ShowListUsers listUsers={listUsers} goDetailUser={goDetailUser}/>
+            <div className="container px-4 px-lg-5 my-5">
+                <h3>Lista de usuarios</h3>
+                <input type="search" placeholder="Search" onChange={handleSearch}></input>
+                <select>
+                    <option hidden></option>
+                    <option >id</option>
+                    <option>nombre</option>
+                    <option>correo</option>
+                </select>
+                <label>Admin: </label>
+                <select>
+                    <option>True</option>
+                    <option>False</option>
+                </select>
+
+                <ShowListUsers listUsers={list} goDetailUser={goDetailUser} />
+            </div>
         </>
     )
 }
