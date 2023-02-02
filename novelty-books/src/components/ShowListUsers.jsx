66718bf1621279d6a5784673c2a49b68cc317371
Troy Pernía <troy00pernia@gmail.com>
2023-02-02 13:29:08 -0400

    changed components/ShowListUsers: changed function pass for props to "selectUser" (line 1) and use it (line 18)

--- novelty-books/src/components/ShowListUsers.jsx
+++ novelty-books/src/components/ShowListUsers.jsx
@@ -1,4 +1,4 @@
-function ShowListUsers({ listUsers, goDetailUser }) {
+function ShowListUsers({ listUsers, selectUser }) {
     return (
         <>
             <div className="container px-4 px-lg-5 my-5">
@@ -15,7 +15,7 @@ function ShowListUsers({ listUsers, goDetailUser }) {
                     </thead>
                     <tbody className="table-group-divider">
                         {listUsers.map((user, index) => (
-                            <tr key={index} onClick={(e) => { goDetailUser(user._id) }} scope="row">
+                            <tr key={index} id={index+1} onClick={(e) => { selectUser(index+1, user) }} scope="row" className={""}>
                                 <th scope="row">{index + 1}</th>
                                 <td>{user._id}</td>
                                 <td>{user.name}</td>
