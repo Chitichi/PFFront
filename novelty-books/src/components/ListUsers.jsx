6235f34cb07d4b6fd6bb2da8c5ae93cc6057f974
Troy Pernía <troy00pernia@gmail.com>
2023-02-02 15:15:34 -0400

    changed components/ListUsers: add localState "typeFilter" (line 12) and function "handleTypeFilter" (line 37)

--- novelty-books/src/components/ListUsers.jsx
+++ novelty-books/src/components/ListUsers.jsx
@@ -9,6 +9,7 @@ function ListUsers({ listUsers }) {
 
     const [list, setList] = React.useState(listUsers)
     const [listToShow, setListToShow] = React.useState(list)
+    const [typeFilter, setTypeFilter] = React.useState("todos")
     const [userSelected, setUserSelected] = React.useState({})
     const [inputSearch, setInputSearch] = React.useState("")
     const router = useRouter()
@@ -18,27 +19,34 @@ function ListUsers({ listUsers }) {
     }
 
     function selectUser(id, user) {
-        setUserSelected({id, user})
+        setUserSelected({ id, user })
         if (userSelected.id) {
             const rowBefore = document.getElementById(userSelected.id)
-            rowBefore.className=""
+            rowBefore.className = ""
         }
         const rowCurrent = document.getElementById(id)
-        rowCurrent.className="bg-warning"
+        rowCurrent.className = "bg-warning"
     }
 
-    function handleSearch(event){
+    function handleSearch(event) {
         const input = event.target.value
         setInputSearch(input)
         filterList(input)
     }
 
-    function filterList(input){
-        const newList = list.filter((user) => {
-            const arrayTrue = propertiesToShow.map(prop => user[prop].includes(input)? true: false)
-            return arrayTrue.includes(true)? true: false
-        })
-        setListToShow(newList)
+    function handleTypeFilter(event) {
+        const { value } = event.target
+        setTypeFilter(value)
+    }
+
+    function filterList(input) {
+        if (typeFilter === "todos") {
+            const newList = list.filter((user) => {
+                const arrayTrue = propertiesToShow.map(prop => user[prop].includes(input) ? true : false)
+                return arrayTrue.includes(true) ? true : false
+            })
+            setListToShow(newList)
+        }
     }
 
     function filterAdmin() {
@@ -50,11 +58,11 @@ function ListUsers({ listUsers }) {
             <div className="container px-4 px-lg-5 my-5">
                 <h3>Lista de usuarios</h3>
                 <input type="search" placeholder="Search" onChange={handleSearch}></input>
-                <select>
-                    <option hidden></option>
-                    <option >id</option>
-                    <option>nombre</option>
-                    <option>correo</option>
+                <select onChange={handleTypeFilter}>
+                    <option value={"todos"}>todos</option>
+                    <option value={"id"}>id</option>
+                    <option value={"nombre"}>nombre</option>
+                    <option value={"corre"}>correo</option>
                 </select>
                 <label>Admin: </label>
                 <select>
