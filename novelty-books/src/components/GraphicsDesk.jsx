5b66089646a25fea92a4cceb7a78048184e9657a
Troy Pernía <troy00pernia@gmail.com>
2023-02-05 12:37:59 -0400

    add components/GraphicsDesk

---
+++ novelty-books/src/components/GraphicsDesk.jsx
@@ -0,0 +1,18 @@
+export default function GraphicsDesk() {
+    return (
+        <>
+            <div>
+                <h2>Contenedor de graficos</h2>
+                <div>
+                    <h3>Grafico de dona</h3>
+                </div>
+                <div>
+                    <h4>Grafico de barra</h4>
+                </div>
+                <div>
+                    <h4>Grafico de linea</h4>
+                </div>
+            </div>
+        </>
+    )
+}
