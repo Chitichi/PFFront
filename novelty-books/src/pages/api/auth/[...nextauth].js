b6a601297b180866da158b5974fbb762fbc4c7e9
LeandroLicata <leandrolicata1@gmail.com>
2023-02-05 12:33:04 -0300

    NEXT_PUBLIC

--- novelty-books/src/pages/api/auth/[...nextauth].js
+++ novelty-books/src/pages/api/auth/[...nextauth].js
@@ -8,8 +8,10 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_CONFIG_GOOGLE_ID,
-      clientSecret: process.env.NEXT_CONFIG_GOOGLE_SECRET,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
+      callbackUrl: "https://novelty-books.vercel.app/api/auth/callback/google",
+      // callbackUrl: "https://localhost:3000/api/auth/callback/google",
     }),
   ],
 };
