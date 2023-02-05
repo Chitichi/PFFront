bb730f9ed3c6529d17c7e9bdef6e8280958e363b
Troy Pernía <troy00pernia@gmail.com>
2023-02-05 16:40:11 -0400

    changed components/GraphicsDesk: order the components to render

--- novelty-books/src/components/GraphicsDesk.jsx
+++ novelty-books/src/components/GraphicsDesk.jsx
@@ -7,11 +7,22 @@ export default function GraphicsDesk({ listOrders, listUsers, listBooks }) {
 
     return (
         <>
-            <div>
-                <h3>Contenedor de graficos</h3>
-                <DoughnutGraph></DoughnutGraph>
-                <BarGraph></BarGraph>
-                <LineGraph></LineGraph>
+            <div className="container">
+                {/* <h3>Contenedor de graficos</h3> */}
+                <div className='row'>
+                    <div className='col-6'>
+                        <DoughnutGraph />
+                    </div>
+                    <div className='col-6'>
+                        <BarGraph listBooks={listBooks} />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-8 justify-content-center align-items-center'>
+                        <LineGraph></LineGraph>
+                    </div>
+                </div>
+
             </div>
         </>
     )
