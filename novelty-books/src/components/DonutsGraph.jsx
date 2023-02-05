d30e573af87fe73583184b42235d050126f310b7
Troy Pernía <troy00pernia@gmail.com>
2023-02-05 18:28:21 -0400

    add components/DonutsGraph

---
+++ novelty-books/src/components/DonutsGraph.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import {genre, selectColors} from "./GraphicsDesk"
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export default function DonutsGraph({listData}) {
+
+    const [color, border] = selectColors()
+
+    const dataDoughnut = {
+        labels: genre,
+        datasets: [
+            {
+                label: '# of Sales',
+                data: listData,
+                backgroundColor: color,
+                borderColor: border,
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    return (
+        <>
+            <div>
+                <h5>Grafico de tipo dona</h5>
+                <Doughnut data={dataDoughnut} />
+            </div>
+        </>
+    )
+} 
