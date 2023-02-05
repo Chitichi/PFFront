f518840ffc1d369c79abe54742edf1f1f04c1259
Troy Pernía <troy00pernia@gmail.com>
2023-02-05 13:25:54 -0400

    changed components/GraphicsDesk: add new tags to render (line 10), and destructurin propertios from props (line 1)

--- novelty-books/src/components/GraphicsDesk.jsx
+++ novelty-books/src/components/GraphicsDesk.jsx
@@ -1,17 +1,27 @@
-export default function GraphicsDesk() {
+export default function GraphicsDesk({ listOrders, listUsers, listBooks }) {
+    // console.log("lista de ordenes: ",listOrders)
+    // console.log("lista de usuarios",listUsers)
+    // console.log("lista de libros", listBooks)
+    // listUsers.lenght? console.log("Usurios -------------------:", listUsers):console.log("no llegaron Usuarios")
+    // listBooks.lenght? console.log("Libros --------------------:", listBooks):console.log("no llegaron Libros")
     return (
         <>
             <div>
-                <h2>Contenedor de graficos</h2>
-                <div>
-                    <h3>Grafico de dona</h3>
-                </div>
-                <div>
-                    <h4>Grafico de barra</h4>
-                </div>
-                <div>
-                    <h4>Grafico de linea</h4>
-                </div>
+                <section>
+                    <h3>Contenedor de graficos</h3>
+                    <figure>
+                        <h5>Grafico de dona</h5>
+                        <canvas id="cakeGraph"></canvas>
+                    </figure>
+                    <figure>
+                        <h5>Grafico de barra</h5>
+                        <canvas id="barGraph"></canvas>
+                    </figure>
+                    <figure>
+                        <h5>Grafico de linea</h5>
+                        <canvas id="lineGraph"></canvas>
+                    </figure>
+                </section>
             </div>
         </>
     )
