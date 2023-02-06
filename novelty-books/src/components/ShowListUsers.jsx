9c3385e9e17172b1b3b4b7384a448eccb704da79
Troy Pernía <troy00pernia@gmail.com>
2023-02-06 16:20:32 -0400

    changed components/ShowListUsers: add scroll to table (line 5)

--- novelty-books/src/components/ShowListUsers.jsx
+++ novelty-books/src/components/ShowListUsers.jsx
@@ -2,31 +2,33 @@ function ShowListUsers({ listUsers, selectUser, goDetailUser }) {
     return (
         <>
             <div className="container px-4 px-lg-5 my-5">
-                <table className="table table-striped table-hover table-bordered ">
-                
-                    <thead className="table-dark">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">id</th>
-                            <th scope="col">nombre</th>
-                            <th scope="col">correo</th>
-                            <th scope="col">admin</th>
-                        </tr>
-                    </thead>
-                    <tbody className="table-group-divider">
-                        {listUsers.map((user, index) => (
-                            <tr key={index} id={index+1} scope="row" className={""}
-                            onClick={(e) => { selectUser(index+1, user) }}
-                            onDoubleClick={() => {goDetailUser(user._id)}}>
-                                <th scope="row">{index + 1}</th>
-                                <td>{user._id}</td>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>{`${user.rolAdmin}`}</td>
+                <div style={{ "overflow": "auto", "height": "450px" }}>
+                    <table className="table table-striped table-hover table-bordered" >
+
+                        <thead className="table-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">id</th>
+                                <th scope="col">nombre</th>
+                                <th scope="col">correo</th>
+                                <th scope="col">admin</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody className="table-group-divider">
+                            {listUsers.map((user, index) => (
+                                <tr key={index} id={index + 1} scope="row" className={""}
+                                    onClick={(e) => { selectUser(index + 1, user) }}
+                                    onDoubleClick={() => { goDetailUser(user._id) }}>
+                                    <th scope="row">{index + 1}</th>
+                                    <td>{user._id}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{`${user.rolAdmin}`}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     )
