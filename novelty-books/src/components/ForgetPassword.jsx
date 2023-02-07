2955dd8c949b38055ec3de23de921ad007218944
unknown <giuli.carrera@gmail.com>
2023-02-07 13:43:08 -0300

    src/components/ForgetPassword.jsx

--- novelty-books/src/components/ForgetPassword.jsx
+++ novelty-books/src/components/ForgetPassword.jsx
@@ -1,101 +1,81 @@
-"use client"
+"use client";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import { useStateContext } from "context/StateContext";
 import Swal from "sweetalert2";
 
+const ForgetPassword = ({ userFetch }) => {
+  const { user, setUser } = useStateContext();
+  const router = useRouter();
+  const [password, setPassword] = useState("");
+  const [confPass, setConfPass] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (password === confPass) {
+        const dataUser = new FormData();
+        dataUser.append("userId", userFetch._id);
+        dataUser.append("password", password);
+        const id = userFetch._id;
 
-const ForgetPassword = ()=>{
-    const {user , setUser} = useStateContext();
-    const router = useRouter();
-    const [botonOff, setBotonOff] = useState(true)
-    const [input, setInput] = useState({
-        userId: user._id,
-        email: user.email,
-        password: user.password,
-    });
-    //console.log(input, "hola soy tu puto input");
-        
-    function saveLocalStorage(user) {
-        localStorage.setItem("user", JSON.stringify(user));
-      }
-    
-        function handleChange(e){
-        e.preventDefault();
-        setInput({ 
-            ...input, 
-            [e.target.name]:e.target.value
-        })
+        const res = await fetch(process.env.RUTA_BACK + `/users/${id}`, {
+          method: "PUT",
+          body: dataUser,
+        });
+        const data = await res.json();
+        console.log(data);
+
+        Swal.fire({
+          position: "top",
+          icon: "success",
+          title: "Your changes were made successfully!",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+        router.push("/");
+      } else
+        Swal.fire({
+          position: "top",
+          icon: "error",
+          title: "The passwords do not match!",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      setConfPass("");
+      setPassword("");
+    } catch (err) {
+      console.log(err);
     }
-   
-  //  console.log(input, "soy el input de handle change");
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const id = user._id
-           // console.log(id, "hola soy tu id"); hasta aca todo ok.
-          const res = await fetch(process.env.RUTA_BACK+`/users/${id}`, {
-              method: "PUT",
-              body: JSON.stringify(input),
-              headers: {
-                  "Content-Type": "application/json",
-                },
-            });
-            const data = await res.json();
-          console.log(data);
-          
-            setUser({...user, password: input.password })
-            saveLocalStorage({...user, password: input.password })
-            Swal.fire({
-            position: 'top',
-            icon: 'success',
-            title: 'Your changes were made successfully!',
-            showConfirmButton: false,
-            timer: 2000
-          })
-          router.push("/")
-        } catch (err) { 
-            console.log(err)
-            }
-            return 
-        }
+    return;
+  };
 
-    return (
-        
-        <form onSubmit={(e) => handleSubmit(e)}>
-
-        <div>
-            <label>New Password:</label>
-            <input
-            onChange={(e) => handleChange(e)}
-            type= "password"
-            placeholder='password...'
-            value = {input.password}
-            name = "password"
-            />
-        </div>
-        <div>
-            <label>Confirm New Password:</label>
-            <input
-            onChange={(e) => handleChange(e)}
-            type= "password"
-            placeholder='confirmPassword...'
-            value = {input.newPassword}
-            name = "confirmPassword"
-            />
-        </div>
-        <div>
-        <button
-              type='submit'>
-                Edit
-            </button>
-        </div>
-        </form>
-    )
-
-    }
-    
-
-
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <div>
+        <label>New Password:</label>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="password..."
+          value={password}
+          name="password"
+        />
+      </div>
+      <div>
+        <label>Confirm New Password:</label>
+        <input
+          onChange={(e) => setConfPass(e.target.value)}
+          type="password"
+          placeholder="confirmPassword..."
+          value={confPass}
+          name="confirmPassword"
+        />
+      </div>
+      <div>
+        <button type="submit">Edit</button>
+      </div>
+    </form>
+  );
+};
 
 export default ForgetPassword;
