bf16cd99484af93750b47437babde62b8e849fd5
Emanuel Zani <92406880+Emanuel-Zani@users.noreply.github.com>
2023-02-08 17:18:53 -0300

    Update page.jsx

--- novelty-books/src/app/page.jsx
+++ novelty-books/src/app/page.jsx
@@ -4,7 +4,7 @@ const Landing = dynamic(()=>import("@/components/Landing"),{ssr:false})
 //import Landing from "@/components/Landing";
 
 const fetchBooks = async () => {
-  const res = await fetch(process.env.RUTA_BACK+"/books");
+  const res = await fetch(process.env.RUTA_BACK+"/books",{cache:'no-store',});
     return await res.json();
    // .catch(error => alert(error.message))
 }
